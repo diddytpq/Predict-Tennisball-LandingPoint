310efbcd80d121ca4ce4a052ec7a5b2ab79d040e
Yo Seph <diddytpq5@gmail.com>
2021-10-29 11:01:15 +0900

    211029

--- src/mecanum_robot_gazebo/src/tennis_pingpong3.py
+++ src/mecanum_robot_gazebo/src/tennis_pingpong3.py
@@ -8,7 +8,7 @@ from tool.pingpong_utils_ver3 import *
 roslib.load_manifest('mecanum_robot_gazebo')
 
 
-def callback_landing_point(data):
+"""def callback_landing_point(data):
     global esti_ball_landing_point
 
     if len(data.data) < 1 :
@@ -17,7 +17,7 @@ def callback_landing_point(data):
     #esti_ball_landing_point = [data.data[0], data.data[1], data.data[2]]
 
     esti_ball_landing_point = list(data.data)
-
+"""
 
 
 
@@ -38,7 +38,7 @@ if __name__ == '__main__' :
     mecanum_L.del_ball()
     mecanum_R.del_ball() 
 
-    add_catch_point = 1
+    add_catch_point = 3
 
     if mod == 0:
         while True:
@@ -68,13 +68,15 @@ if __name__ == '__main__' :
             #print(ball_landing_point)
             #print(add_catch_point * np.cos(mecanum_0.yaw_z),add_catch_point * np.sin(mecanum_0.yaw_z))
 
-            rospy.Subscriber("esti_landing_point", Float64MultiArray, callback_landing_point)
-            ball_landing_point = [mecanum_L.x_target + add_catch_point * np.cos(mecanum_L.yaw_z), mecanum_L.y_target + add_catch_point * np.sin(mecanum_L.yaw_z)]
+            #rospy.Subscriber("esti_landing_point", Float64MultiArray, callback_landing_point)
+            #ball_landing_point = [mecanum_L.x_target + add_catch_point * np.cos(mecanum_L.yaw_z), mecanum_L.y_target + add_catch_point * np.sin(mecanum_L.yaw_z)]
 
             mecanum_R.move_base_camera(add_catch_point, mecanum_L)
 
 
             mecanum_R.spwan_ball("ball_right")
             mecanum_R.throw_ball()  
+
             ball_landing_point = [mecanum_R.x_target - add_catch_point * np.cos(mecanum_R.yaw_z), mecanum_R.y_target - add_catch_point * np.sin(mecanum_R.yaw_z)]
+           
             mecanum_L.move(ball_landing_point[0],ball_landing_point[1] ,mecanum_R)
